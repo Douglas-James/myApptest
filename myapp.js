e2f451e08d671022014ef2194fa8ba0390cd65d6
Douglas-James <ryle@drumclog.com>
2023-04-12 13:42:06 -0500

    'test myapp.js'

--- myapp.js
+++ myapp.js
@@ -1,6 +1,10 @@
 const http = require('http');
+// const colors = require('colors')
 const display = require('./routes/route');
 
 const server = http.createServer(display);
 
-server.listen(3001);
+server.listen(3002);
+
+// console.log('inverse the color'.inverse);
+// console.log("hello".blue)
